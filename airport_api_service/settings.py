7c60cbde940269afed435893a4eb01931f1e1f3b
Yuliana <pashkova.yuliana@gmail.com>
2024-03-21 14:29:24 +0200

    add airport & user apps to installed apps in settings.py

--- airport_api_service/settings.py
+++ airport_api_service/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'airport',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'airport_api_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'airport',
+        'USER': 'testuser',
+        'PASSWORD': 'testuser123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
