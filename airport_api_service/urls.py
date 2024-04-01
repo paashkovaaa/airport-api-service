631e4d5b89cdb0a97ad351f4d6576c7374f1187b
Yuliana <pashkova.yuliana@gmail.com>
2024-04-01 11:50:44 +0300

    add urls for redoc & swagger & add urls from user/urls.py & airport/urls.py

--- airport_api_service/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("api/airport/", include("airport.urls", namespace="airport")),
-    path("api/user/", include("user.urls", namespace="user")),
-]
